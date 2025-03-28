00c6a4cd1bc0d5efdf22ed4455ca26d7b38ddb75
Osman Erdem Kilic <osman_erdem1200@hotmail.com>
2025-03-28 14:41:38 +0300

    Servis Düzenlemeleri 2

--- Core/Shoper.Application/Dtos/OrderDtos/UpdateOrderDto.cs
+++ Core/Shoper.Application/Dtos/OrderDtos/UpdateOrderDto.cs
@@ -12,7 +12,7 @@ namespace Shoper.Application.Dtos.OrderDtos
     {
         public int OrderId { get; set; }
         //public DateTime OrderDate { get; set; }
-        public decimal TotalAmount { get; set; }
+        //public decimal TotalAmount { get; set; }
         public string OrderStatus { get; set; }
         //public string BillingAddress { get; set; }
         //public string ShippingAddress { get; set; }
